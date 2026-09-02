ff85affef74fc5408599b13308a0befb80a2347d
Shaeeb Ali Khan <shaeebalikhan@gmail.com>
2026-09-02 18:23:40 +0530

    Fix frontend API URL

--- frontend/app/lib/axios.js
+++ frontend/app/lib/axios.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
-export const axiosInstance = axios.create(
-    {
-        baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000" : "https://chatapp-backend-96a2.onrender.com",
-        withCredentials: true,
-    }
-)
+// export const axiosInstance = axios.create(
+//     {
+//         baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000" : "https://chatapp-backend-96a2.onrender.com",
+//         withCredentials: true,
+//     }
+// )
+
+export const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  withCredentials: true,
+});
