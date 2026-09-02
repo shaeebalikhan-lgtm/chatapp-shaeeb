830c366a8bd9a59dabdb41a054e9db0f58ae8445
Shaeeb Ali Khan <shaeebalikhan@gmail.com>
2026-09-02 17:15:02 +0530

    Prepare backend for production deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,6 @@ import cors from "cors";
 //cors = allows frontend ↔ backend communication.
 //Allow other websites/apps to talk to my backend.
 
-
-
 import { connectDB } from "./lib/db.js";
 import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
@@ -22,13 +20,13 @@ const PORT = Number(process.env.PORT) || 5000;
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
   })
 );
-
 app.use("/auth/api", authRoute);
 app.use("/message/api", messageRoute);
 
